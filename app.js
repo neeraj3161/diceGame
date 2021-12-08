694d5ca2197b36d04f7e60b7d10088f5ce88abc8
Neeraj Tripathi <93697901+neeraj3161@users.noreply.github.com>
2021-12-08 13:22:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+document.querySelector(".btn").addEventListener("click",function(){
+    var randomVar=(Math.random()*5)+1;
+var rounded1=(Math.round(randomVar));
+var randomVar=(Math.random()*5)+1;
+var rounded2=(Math.round(randomVar));
+
+function player1(){
+   document.querySelector('.img1').src="images/dice"+rounded1+".png";
+   return rounded1;
+}
+function player2(){
+   document.querySelector('.img2').src="images/dice"+rounded2+".png";
+   return rounded2;
+}
+function won(){
+    if (rounded1>rounded2){
+        document.querySelector(".won").innerHTML="<h3>Player 1 Won!!</h3><p>👏</p>"
+    }else if(rounded1==rounded2){
+        document.querySelector(".won").innerHTML="<h3>It's a Draw!! Try again</h3><p>😂</p>"
+    }
+    
+}
+    player1();
+    player2();
+    won();
+})
+
+
